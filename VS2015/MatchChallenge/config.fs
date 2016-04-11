82b025e63dfa44e1337ebe68a360a82819706712
Justin <justinjohnwilliams@gmail.com>
2016-04-11 09:47:55 -0500

    VS solution

---
+++ VS2015/MatchChallenge/config.fs
@@ -0,0 +1,13 @@
+﻿module config
+
+  let matchUrl = "http://www.match.com"
+  let matchCreateAccountUrl = "http://www.match.com/registration/registration.aspx"
+  let loginUrl = "https://secure.match.com/login/index/#/"
+  let profileWelcomeVerifyUrl = "/Profile/Create/Welcome/?"
+  let myMatchVerify1Url = "/home/mymatch.aspx"
+  let myMatchVerify2Url = "/MyMatch/Index"
+  let mutable siteType = 1
+  let mutable myFavorite = ""
+  let mutable email = ""
+  let mutable username = ""
+  let mutable password = ""
